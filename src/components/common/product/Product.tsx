49784ab9695ae663492a57dd596d1495f4ad08cd
NgocMinh10122001 <phamngocminh10122001@gmail.com>
2024-08-27 18:26:26 +0700

    no cap

--- src/components/common/product/Product.tsx
+++ src/components/common/product/Product.tsx
@@ -4,37 +4,35 @@ import { useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
 import { FaStar } from "react-icons/fa6";
 
-function Product({ product }: { product: IProduct }) {
-  const [height, setHeight] = useState(window.innerWidth);
+interface ProductProps {
+  product: IProduct;
+}
+
+const Product: React.FC<ProductProps> = ({ product }) => {
+  const [height, setHeight] = useState<number>(0); // Initialize with a default value
   const router = useRouter();
 
   useEffect(() => {
     const handleResize = () => setHeight(window.innerWidth);
 
+    // Set initial height
+    handleResize();
+
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
   }, []);
+
   return (
     <div
       className="product w-full overflow-hidden cursor-pointer"
       onClick={() => router.push(`/san-pham/${product.id}`)}
     >
       <div
-        className={`zoomOut group bg-no-repeat w-full   cursor-pointer relative`}
+        className={`zoomOut group bg-no-repeat w-full cursor-pointer relative`}
+        style={{
+          height: height > 678 ? 407 : height * 0.52994791666,
+        }}
       >
-        {/* background-image: url("${product.thumbNailZoomOut}"); */}
-        {/* .zoomOut:hover {
-              background-image: url("${product.thumbNailZoomIn}");
-            } */}
-        <style jsx>
-          {`
-            .zoomOut {
-              height: ${window.innerWidth > 678
-                ? `407`
-                : `${height * 0.52994791666}`}px;
-            }
-          `}
-        </style>
         <img
           src={`${product.thumbNail[0]}`}
           alt="no-img"
@@ -43,18 +41,18 @@ function Product({ product }: { product: IProduct }) {
         <img
           src={`${product.thumbNail[1]}`}
           alt="no-img"
-          className={`absolute top-0 bottom-0 w-full  left-0 right-0 opacity-0 group-hover:opacity-100 h-full object-cover duration-1000 ease-in-out`}
+          className={`absolute top-0 bottom-0 w-full left-0 right-0 opacity-0 group-hover:opacity-100 h-full object-cover duration-1000 ease-in-out`}
         />
         {product?.status === "off" && (
           <div className="status absolute w-full h-fit py-5 bg-white opacity-70 top-[40%] text-center">
-            <span className="uppercase text-black text-lg font-semibold ">
+            <span className="uppercase text-black text-lg font-semibold">
               Hết hàng
             </span>
           </div>
         )}
         {product?.status === "sell" && (
           <div className="status absolute w-fit h-fit p-2 bg-red-700 opacity-70 shadow-lg shadow-red-500 top-0 right-0 text-center">
-            <span className="uppercase text-white text-base font-semibold ">
+            <span className="uppercase text-white text-base font-semibold">
               Giảm giá
             </span>
           </div>
@@ -67,11 +65,11 @@ function Product({ product }: { product: IProduct }) {
             {product?.des}
           </p>
         </div>
-        <div className="bot pt-2 ">
-          <p className="flex items-center justify-center ">
+        <div className="bot pt-2">
+          <p className="flex items-center justify-center">
             {Array(product?.rate)
               .fill("")
-              .map((item, index) => (
+              .map((_, index) => (
                 <FaStar key={index} size={16} color="#d35400" />
               ))}
           </p>
@@ -99,6 +97,6 @@ function Product({ product }: { product: IProduct }) {
       </div>
     </div>
   );
-}
+};
 
 export default Product;
